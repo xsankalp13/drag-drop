edde30541cdc0bc0ab98aef7b1ea6996c2091635
Sankalp Salve <sankalpsalve.en21156180@dypisp.onmicrosoft.com>
2023-06-09 07:56:06 +0530

    some minute changes

--- script.js
+++ script.js
@@ -34,17 +34,12 @@ const reset = () => {
     draggables.forEach(draggable => {
         containers[0].appendChild(draggable);
     })
-    containers.forEach(cont => {
-        if(cont.childElementCount == 1){
-            cont.style.backgroundColor = "rgb(255, 252, 81)";
-            cont.style.transition = "0.5s ease";
-        }
-        else if(cont.childElementCount == 0){
-            cont.style.backgroundColor = "rgb(255, 81, 81)";
-            cont.style.transition = "0.5s ease";
-        }else if(cont.childElementCount == 2){
-            cont.style.backgroundColor = "rgb(105, 105, 255)";
-            cont.style.transition = "0.5s ease";
-        }
-    })
+    containers[0].style.backgroundColor = "rgb(105, 105, 255)";
+    containers[0].style.transition = "0.5s ease";
+
+    containers[1].style.backgroundColor = "rgb(255, 81, 81)";
+    containers[1].style.transition = "0.5s ease";
+
+
+
 }
